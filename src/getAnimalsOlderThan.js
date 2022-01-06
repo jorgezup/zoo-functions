ec26dd44a737c36cbc667ef99294c234016d6a2e
Jorge Zupirolli <jorgebertolo@gmail.com>
2022-01-06 17:37:26 -0300

    Requisito 02

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,10 @@
 const data = require('../data/zoo_data');
 
 function getAnimalsOlderThan(animal, age) {
-  // seu código aqui
+  const filteredAnimals = data.species
+    .filter((item) => item.name === animal)
+    .map((item) => item.residents);
+  return filteredAnimals[0].every((ele) => ele.age > age);
 }
 
 module.exports = getAnimalsOlderThan;
