636d4136723e7edf72774978a95051d9742fd882
Jorge Zupirolli <jorgebertolo@gmail.com>
2022-01-09 12:34:33 -0300

    Requisito 05

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -7,12 +7,18 @@ function countAnimals(animal) {
       return accumulator;
     }, {});
   }
-  if (animal.specie) {
-    return species.filter((specie) => specie.name === animal.specie)[0]
-      .residents.length;
+  /* Faz a busca da specie pelo nome passado como parâmetro */
+  const animalSpecieFounded = species.find(
+    (specie) => specie.name === animal.specie,
+  );
+  /*  Caso tenha o sexo no parêmetro entra neste if */
+  if (animal.sex) {
+    return animalSpecieFounded.residents.filter(
+      (resident) => resident.sex === animal.sex,
+    ).length;
   }
+  /* Caso tenha apenas a specie retorna a linha abaixo */
+  return animalSpecieFounded.residents.length;
 }
-
-console.log(countAnimals({ specie: 'penguins' }));
 
 module.exports = countAnimals;
