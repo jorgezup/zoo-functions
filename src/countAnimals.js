b1c3a0db833bd634e80058a144853024bcd83dc7
Jorge Zupirolli <jorgebertolo@gmail.com>
2022-01-09 11:39:52 -0300

    em desenvolvimento requisito 5

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,16 @@
-const data = require('../data/zoo_data');
+const { species } = require('../data/zoo_data');
 
 function countAnimals(animal) {
-  // seu código aqui
+  if (!animal) {
+    return species.reduce((accumulator, currentSpecie) => {
+      accumulator[currentSpecie.name] = currentSpecie.residents.length;
+      return accumulator;
+    }, {});
+  }
+
+  return animal;
 }
+
+console.log(countAnimals());
 
 module.exports = countAnimals;
