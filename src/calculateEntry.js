a024cb2f078e685979f6956fa350df60b5a627bd
Jorge Zupirolli <jorgebertolo@gmail.com>
2022-01-10 09:11:42 -0400

    Requisito 06: retorna 0 para objeto vazio e caso nao seja passado parametro

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,12 +1,11 @@
 const data = require('../data/zoo_data');
 
 function countEntrants(entrants) {
-  // seu código aqui
-  //
+  if (!entrants || Object.keys(entrants).length === 0) return 0;
 }
 
 function calculateEntry(entrants) {
-  // seu código aqui
+  if (!entrants || Object.keys(entrants).length === 0) return 0;
 }
 
 module.exports = { calculateEntry, countEntrants };
