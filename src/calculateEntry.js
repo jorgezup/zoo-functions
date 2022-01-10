31a90a36408554a41019ca5c23bf4d1688659793
Jorge Zupirolli <jorgebertolo@gmail.com>
2022-01-10 09:48:30 -0400

    Requisito 06: retorna o array com a contagem de visitantes

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,20 +1,27 @@
+/* eslint-disable max-lines-per-function */
 const data = require('../data/zoo_data');
 
 function countEntrants(entrants) {
   if (!entrants || Object.keys(entrants).length === 0) return 0;
-  return entrants.reduce((accumulator, currentPerson) => {
-    let child = 0;
-    let adult = 0;
-    let senior = 0;
-    if (currentPerson.age < 18) {
-      child += 1;
-    }
-    if (currentPerson.age >= 18 && currentPerson < 50) {
-      adult += 1;
-    }
-    senior += 1;
-    return { `child: ${child} adult: ${adult} senior: ${senior} `};
-  });
+  return entrants.reduce(
+    (accumulator, currentPerson) => {
+      if (currentPerson.age < 18) {
+        accumulator.child += 1;
+      }
+      if (currentPerson.age >= 18 && currentPerson.age < 50) {
+        accumulator.adult += 1;
+      }
+      if (currentPerson.age >= 50) {
+        accumulator.senior += 1;
+      }
+      return accumulator;
+    },
+    {
+      child: 0,
+      adult: 0,
+      senior: 0,
+    },
+  );
 }
 
 function calculateEntry(entrants) {
