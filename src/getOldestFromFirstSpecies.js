b5f09f571e8def9b2ff5a08aebfbd70d2ee5c577
Jorge Zupirolli <jorgebertolo@gmail.com>
2022-01-10 19:07:02 -0400

    Requisito 09

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -10,6 +10,4 @@ function getOldestFromFirstSpecies(id) {
   return [name, sex, age];
 }
 
-console.log(getOldestFromFirstSpecies('c5b83cb3-a451-49e2-ac45-ff3f54fbe7e1'));
-
 module.exports = getOldestFromFirstSpecies;
