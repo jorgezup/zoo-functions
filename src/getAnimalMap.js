4f34ee9c996f8708243d277d05d22d77d20db952
Jorge Zupirolli <jorgebertolo@gmail.com>
2022-01-10 19:04:56 -0400

    Requisito 09

--- src/getAnimalMap.js
+++ src/getAnimalMap.js
@@ -1,3 +1,5 @@
+/* eslint-disable max-lines-per-function */
+/* eslint-disable complexity */
 const { species } = require('../data/zoo_data');
 
 const reducer = (accumulator, current) => {
@@ -9,10 +11,58 @@ const reducer = (accumulator, current) => {
   return accumulator;
 };
 
+// const reducerWithNames = (accumulator, current) => {
+//   if (accumulator[current.location] === undefined) {
+//     accumulator[current.location] = [];
+//   }
+
+//   accumulator[current.location].push({
+//     [current.name]: current.residents.map((item) => item.name),
+//   });
+
+//   return accumulator;
+// };
+
+const reducerSorted = (accumulator, current) => {
+  if (accumulator[current.location] === undefined) {
+    accumulator[current.location] = [];
+  }
+
+  accumulator[current.location].push({
+    [current.name]: current.residents
+      .map((item) => item)
+      .filter((item) => item.sex === sex)
+      .map((item) => item.name)
+      .sort(),
+  });
+
+  console.dir('accumulator', accumulator, { depth: 4 });
+
+  return accumulator;
+};
+
 function getAnimalMap(options) {
   if (!options) {
     return species.reduce(reducer, {});
   }
+  // if (options.includesNames) {
+  //   return species.reduce(reducerWithNames, {});
+  // }
+  if (options.sex) {
+    const a = species
+      .map((item) => item)
+      .map((item) => item.residents.sex === 'male');
+
+    console.log(a);
+  }
 }
+
+const options = {
+  sorted: true,
+  includesNames: true,
+  sex: 'female',
+};
+
+console.dir(getAnimalMap(options), { depth: 4 });
 
 module.exports = getAnimalMap;
--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,15 @@
-const data = require('../data/zoo_data');
+const { employees, species } = require('../data/zoo_data');
 
 function getOldestFromFirstSpecies(id) {
-  // seu código aqui
+  const getAnimalId = employees.find((employee) => employee.id === id)
+    .responsibleFor[0];
+  const getResidents = species
+    .filter((animal) => animal.id === getAnimalId)[0]
+    .residents.sort((a, b) => b.age - a.age)[0];
+  const { name, age, sex } = getResidents;
+  return [name, sex, age];
 }
+
+console.log(getOldestFromFirstSpecies('c5b83cb3-a451-49e2-ac45-ff3f54fbe7e1'));
 
 module.exports = getOldestFromFirstSpecies;
