784b00cc3d3caaa8a307f83684c03614d00ac74a
Jorge Zupirolli <jorgebertolo@gmail.com>
2022-01-11 18:09:12 -0400

    Requisito 08: finalizado; necessita melhorias

--- src/getSchedule.js
+++ src/getSchedule.js
@@ -1,41 +1,42 @@
+/* eslint-disable complexity */
 /* eslint-disable max-lines-per-function */
+/* eslint-disable no-confusing-arrow */
 /* eslint-disable editorconfig/editorconfig */
 const { species, hours } = require('../data/zoo_data');
 
 function getOfficeHoursWithoutTarget(officeHours, speciesDaysAvailable) {
-  return officeHours.sort().map((day) => ({
-    // ordena o array de dias e para cada dia é criado um objeto
-    [day[0]]: {
-      // faz a verificação caso seja segunda coloque como fechado,
-      // caso contrário utiliza as informações de abertura e fechamento
+  return officeHours.sort().map(([day, openCloseHoursEachDay]) => ({
+    [day]: {
       officeHour:
-        day[0] === 'Monday'
+        day === 'Monday'
           ? 'CLOSED'
-          : `Open from ${day[1].open}am until ${day[1].close}pm`,
-      // utiliza a mesma lógica de cima
+          : `Open from ${openCloseHoursEachDay.open}am until ${openCloseHoursEachDay.close}pm`,
       exhibition:
-        day[0] === 'Monday'
+        day === 'Monday'
           ? 'The zoo will be closed!'
-          : speciesDaysAvailable /* 
-          [
-            [ 'lions', [ 'Tuesday', 'Thursday', 'Saturday', 'Sunday' ] ],
-            ...
-          ]
-           */
-              .map((item) => {
-                /* 
-                  Verifica se os dias da espécie e igual o dia de exibição
-                  caso seja adiciona a espécie, caso não retorna false
-                  no final é feito um filtro para remover o false
-                */
-                if (item[1].includes(day[0])) {
-                  return item[0];
-                }
-                return false;
-              })
+          : speciesDaysAvailable
+              .map(([animal, arrayOfDayEachAnimal]) =>
+                arrayOfDayEachAnimal.includes(day) ? animal : false,
+              )
               .filter((item) => item !== false),
     },
   }));
+}
+
+function getOfficeHoursSpecificDay(officeHours, speciesDaysAvailable, param) {
+  const filtered = officeHours.find(([day]) => day === param);
+  const [day, openCloseHours] = filtered;
+
+  return {
+    [day]: {
+      officeHour: `Open from ${openCloseHours.open}am until ${openCloseHours.close}pm`,
+      exhibition: speciesDaysAvailable
+        .map(([animal, arrayOfDayEachAnimal]) =>
+          arrayOfDayEachAnimal.includes(day) ? animal : false,
+        )
+        .filter((item) => item !== false),
+    },
+  };
 }
 
 function getAnimalsAndDays() {
@@ -44,21 +45,16 @@ function getAnimalsAndDays() {
   return [...animals, ...days];
 }
 
-function getSchedule(scheduleTarget) {
-  /* 
-    Transforma o objeto em um array
-    sendo a posição [0] do array o dia da semana
-    e a posição [1] do array um objeto contendo open e close
-   */
-  const arrayOfOfficeHours = Object.entries(hours);
-  /* 
-    Cria um array com o nome da espécie e os dias disponíveis
-  */
-  const arraySpecieDaysAvialable = species.map((specie) => [
-    specie.name,
-    specie.availability,
-  ]);
+const arrayOfOfficeHours = Object.entries(hours);
+const arraySpecieDaysAvialable = species.map((specie) => [
+  specie.name,
+  specie.availability,
+]);
 
+const isDay = (param) => Object.entries(hours).some((day) => day[0] === param);
+const isAnimal = (param) => species.find((specie) => specie.name === param);
+
+function getSchedule(scheduleTarget) {
   const withoutTarget = getOfficeHoursWithoutTarget(
     arrayOfOfficeHours,
     arraySpecieDaysAvialable,
@@ -69,17 +65,26 @@ function getSchedule(scheduleTarget) {
   );
 
   if (!scheduleTarget || !targetIsCorrect) {
-    /* Utilizei o reduce para retornar apenas um objeto */
     return withoutTarget.reduce((acc, curr) => ({ ...acc, ...curr }));
   }
-
   if (scheduleTarget === 'Monday') {
     return {
       Monday: { officeHour: 'CLOSED', exhibition: 'The zoo will be closed!' },
     };
   }
-
-  // console.dir(daysOfficeHours, { depth: 4 });
+  if (isAnimal(scheduleTarget)) return isAnimal(scheduleTarget).availability;
+  if (isDay(scheduleTarget)) {
+    return getOfficeHoursSpecificDay(
+      arrayOfOfficeHours,
+      arraySpecieDaysAvialable,
+      scheduleTarget,
+    );
+  }
+  // const specieAvailability = species.find(
+  //   (item) => item.name === scheduleTarget,
+  // ).availability;
+  // if (specieAvailability) return specieAvailability;
+  // console.dir('asdas', Object.keys(withoutTarget), { depth: 4 });
   // return daysOfficeHours;
 }
 
