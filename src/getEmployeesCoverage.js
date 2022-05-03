3c2ac84d3d597d5fc7b8eeea402b26a5b1ea458a
Jorge Zupirolli <jorgebertolo@gmail.com>
2022-05-03 11:18:55 -0400

    update readme

--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -1,7 +1,27 @@
-const data = require('../data/zoo_data');
+/* eslint-disable max-lines-per-function */
+/* eslint-disable no-unused-expressions */
+const { species, employees } = require('../data/zoo_data');
+
+const getSpecies = (responsible) =>
+  responsible.reduce((accu, animalId) => {
+    const { name, location } = species.find((specie) => specie.id === animalId);
+    accu.push({ specie: name, location });
+    return accu;
+  }, []);
 
 function getEmployeesCoverage() {
-  // seu código aqui
+  return employees.reduce((acc, employee) => {
+    const { id, firstName, lastName, responsibleFor } = employee;
+    acc.push({
+      id,
+      fullName: `${firstName} ${lastName}`,
+      species: getSpecies(responsibleFor).map((item) => item.specie),
+      locations: getSpecies(responsibleFor).map((item) => item.location),
+    });
+    return acc;
+  }, []);
 }
+
+console.dir(getEmployeesCoverage(), { depth: 4 });
 
 module.exports = getEmployeesCoverage;
